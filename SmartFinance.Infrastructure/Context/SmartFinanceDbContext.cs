c7a0450fd3677c6e2fbbde89973f102416e0ff7e
Mehmet Koç <mehmet.koc.brs1453@gmail.com>
2026-02-14 00:18:53 +0300

    feat: Infrastructure katmanı - EF Core kurulumu ve SmartFinanceDbContext eklendi

---
+++ SmartFinance.Infrastructure/Context/SmartFinanceDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using SmartFinance.Domain.Entities;
+
+namespace SmartFinance.Infrastructure.Context;
+
+public class SmartFinanceDbContext : DbContext
+{
+    public SmartFinanceDbContext(DbContextOptions<SmartFinanceDbContext> options) : base(options)
+    {}
+    public DbSet<User> Users {get;set;}
+    public DbSet<Transaction> Transactions {get;set;}
+    public DbSet<Category> Categories {get;set;}
+
+}
